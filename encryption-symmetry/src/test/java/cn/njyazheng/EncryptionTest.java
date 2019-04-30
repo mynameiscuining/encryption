f54c30f50fcf2888cbdea49dfb6d68db192d05a5
cuining <cuining@asiainfo-sec.com>
2019-04-30 09:19:53 +0800

    3des测试

--- encryption-symmetry/src/test/java/cn/njyazheng/EncryptionTest.java
+++ encryption-symmetry/src/test/java/cn/njyazheng/EncryptionTest.java
@@ -32,7 +32,27 @@ public class EncryptionTest {
         System.out.println("BouncyCastleDES加密之后为:" + target);
         System.out.println("BouncyCastleDES解密之后为:" + BouncyCastleDES.decrypt(target,bytes));
     }
-
+    @Test
+    public void compareDES1() throws Exception {
+        byte[] bytes=JDKDES.getKey();
+        String target = JDKDES.encrypt(SOURCE,bytes);
+        System.out.println("JDKDES加密之后为:" + target);
+        System.out.println("BouncyCastleDES解密之后为:" + BouncyCastleDES.decrypt(target,bytes));
+    }
+    @Test
+    public void compareDES2() throws Exception {
+        byte[] bytes=BouncyCastleDES.getKey();
+        String target = JDKDES.encrypt(SOURCE,bytes);
+        System.out.println("JDKDES加密之后为:" + target);
+        System.out.println("BouncyCastleDES解密之后为:" + BouncyCastleDES.decrypt(target,bytes));
+    }
+    @Test
+    public void compareDES3() throws Exception {
+        byte[] bytes=BouncyCastleDES.getKey();
+        String target = BouncyCastleDES.encrypt(SOURCE,bytes);
+        System.out.println("BouncyCastleDES加密之后为:" + target);
+        System.out.println("JDKDES解密之后为:" + JDKDES.decrypt(target,bytes));
+    }
     @Test
     public void JDK3DES() throws Exception {
         byte[] bytes=JDK3DES.getKey();
@@ -46,6 +66,14 @@ public class EncryptionTest {
         byte[] bytes=BouncyCastle3DES.getKey();
         String target = BouncyCastle3DES.encrypt(SOURCE,bytes);
         System.out.println("BouncyCastle3DES加密之后为:" + target);
-        System.out.println("JBouncyCastle3DES解密之后为:" + BouncyCastle3DES.decrypt(target,bytes));
+        System.out.println("BouncyCastle3DES解密之后为:" + BouncyCastle3DES.decrypt(target,bytes));
+    }
+
+    @Test
+    public void compare3DES1() throws Exception {
+        byte[] bytes=JDK3DES.getKey();
+        String target = BouncyCastle3DES.encrypt(SOURCE,bytes);
+        System.out.println("BouncyCastle3DES加密之后为:" + target);
+        System.out.println("JDK3DES解密之后为:" + JDK3DES.decrypt(target,bytes));
     }
 }
